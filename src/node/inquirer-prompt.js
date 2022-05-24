7e70c4b6cd8aceaad2993fd92b591866c9b5a6e2
wanyuaning <wanyuaning@163.com>
2022-05-24 17:35:59 +0800

    a

--- src/node/inquirer-prompt.js
+++ src/node/inquirer-prompt.js
@@ -13,6 +13,7 @@ function prompt(type, title, options) {
     })
 }
 
+// const {input, checkbox, radio, confirm, password} = require('inquirer-prompt.js')
 module.exports = {
     /**
      * 复选框
@@ -20,6 +21,10 @@ module.exports = {
      * @param {Array} options 选项集合
      * @param {Array} defaultValue 子选项集合
      * @return {Array} ['复选一']
+     * @example
+     *  checkbox('请勾选下列选项:', ['复选一', '复选二', '复选三']).then(arr => {
+            console.log('复选结果', typeof arr, arr)
+        })
      */
     checkbox: (title, options, defaultValue) => {
         title = title || 'Please multiple choice:'
@@ -35,6 +40,10 @@ module.exports = {
      * @param {Array[Object]} options 
      * @param {Number} defaultValue 数组索引
      * @return {值类型} 2/‘2’
+     * @example
+     *  radio('请选择下列选项:', [{ value: 1, name: '单选一' }, { value: 2, name: '单选二' }]).then(value => {
+            console.log('单选结果', typeof value, value)
+        })
      */
     radio: (title, options, defaultValue) => {
         title = title || 'Please single choice:'
@@ -48,6 +57,10 @@ module.exports = {
      * @param {String} title 
      * @param {Boolean} showMask 是否显示*号
      * @returns {String} '123456'
+     * @example
+     *  password('请输入密码:', true).then(str => {
+            console.log('输入密码', typeof str, str)
+        })
      */
     password: (title, showMask) => {
         title = title || 'Please enter password:'
@@ -58,6 +71,10 @@ module.exports = {
      * @param {String} title 
      * @param {Boolean} defaultValue
      * @returns {Boolean} false
+     * @example
+     *  confirm('请确认！', true).then(bl => {
+            console.log('确认结果', typeof bl, bl)
+        })
      */
     confirm: (title, defaultValue) => {
         title = title || 'Please confirm!'
@@ -68,27 +85,13 @@ module.exports = {
      * @param {String} title 
      * @param {String/Number} defaultValue
      * @returns {value} 输入的都识别为String 默认值为Number时返回Number
+     * @example
+     *  input('请输入:', '默认值').then(value => {
+            console.log('输入结果', typeof value, value)
+        })
      */
     input: (title, defaultValue) => {
         title = title || 'Please input:'
         return prompt('input', title, { default: defaultValue || '' })
     }
 }
-
-// const {input, checkbox, radio, confirm, password} = require('inquirer-prompt.js')
-// input('请输入:', '默认值').then(value => {
-//     console.log('输入结果', typeof value, value)
-// })
-// confirm('请确认！', true).then(bl => {
-//     console.log('确认结果', typeof bl, bl)
-// })
-
-// password('请输入密码:', true).then(str => {
-//     console.log('输入密码', typeof str, str)
-// })
-// checkbox('请勾选下列选项:', ['复选一', '复选二', '复选三']).then(arr => {
-//     console.log('复选结果', typeof arr, arr)
-// })
-// radio('请选择下列选项:', [{ value: 1, name: '单选一' }, { value: 2, name: '单选二' }]).then(value => {
-//     console.log('单选结果', typeof value, value)
-// })
